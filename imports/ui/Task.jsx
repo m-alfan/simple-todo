d18529d4b30519a3bf494fe2e4705a6ed32417e0
Muhamad Alfan <muhamad.alfan01@gmail.com>
2017-01-14 16:37:27 +0700

    bug fix fitur access login sebelum buat to do list

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -33,7 +33,7 @@ export default class Task extends Component {
 				/>
 
 				<span className="text">
-					<strong>{this.props.task.text}</strong>: {this.props.task.text}
+					<strong>{this.props.task.username}</strong>: {this.props.task.text}
 				</span>
 			</li>
 		);
